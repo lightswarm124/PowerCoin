40c272945d9b143d8e21d6c257fbad53ed69141a
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-06-09 01:24:03 -0400

    WIP

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-<<<<<<< HEAD
-
-
-
-
-=======
 var _ = require('underscore');
 var PCM = require('./build/contracts/PowerCoinMeter.sol');
 var pcm = PCM.deployed();
@@ -24,7 +18,6 @@ var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 PCM.setProvider(web3.currentProvider);
 //
->>>>>>> ui
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
@@ -33,24 +26,6 @@ app.get('/', function(req, res){
 
 app.use(express.static('public'));
 
-<<<<<<< HEAD
-//Whenever someone connects this gets executed
-io.on('connection', function(socket){
-  console.log('A user connected');
-  setInterval(function(){
-	  	//Sending an object when emmiting an event
-		volt = Math.floor((Math.random() * 220) + 1);
-		watt = Math.floor((Math.random() * 110) + 1);
-		amps = Math.round(watt/ volt * 100)/100 ;
-		socket.emit('meter_data', {
-			volt : volt,
-			watt : watt,
-			amps : amps,
-			kwh : Math.floor((Math.random() * 10) + 1),
-			timestamp : new Date()/1
-		});
-    }, 4000);
-=======
 function generatePower() {
     var volt = Math.floor((Math.random() * 220) + 1);
     var watt = Math.floor((Math.random() * 110) + 1);
@@ -96,11 +71,6 @@ io.on('connection', function(socket){
 
 
   updateReading();
->>>>>>> ui
-  socket.on('disconnect', function () {
-    console.log('A user disconnected');
-  });
-
   //Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
     console.log('A user disconnected');
